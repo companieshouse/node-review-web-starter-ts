c358cda08ba0ff64955a52992116cd12192c5e36
adeharrison-ch <aharrison1@companieshouse.gov.uk>
2023-05-31 12:57:55 +0100

    Set context root as registered-email-address

--- src/routers/indexRouter.ts
+++ src/routers/indexRouter.ts
@@ -1,10 +1,9 @@
 import { Request, Response, Router, NextFunction } from "express";
 import { HomeHandler } from "./handlers/index/home";
-
 const router: Router = Router();
 const routeViews: string = "router_views/index";
 
-router.get("/", async (req: Request, res: Response, next: NextFunction) => {
+router.get("/registered-email-address", async (req: Request, res: Response, next: NextFunction) => {
     const handler = new HomeHandler();
     const viewData = await handler.execute(req, res);
     res.render(`${routeViews}/home`, viewData);
