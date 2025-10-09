94abd3540f42cf00cb9866869009c722ae71ffed
ch-ncolley <ncolley@companieshouse.gov.uk>
2025-10-09 10:32:10 +0100

    Update eslint
    For now remove standard plugins as it does not work
    well with TypeScript, we will determine best option
    after these changes.

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    rules: {
+      // Temporarily turn off failing rules
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-wrapper-object-types": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-empty-object-type": "off"
+    }
+  }
+);
